e6ccea71d75a8eeff59cd9e4fe78cd66d47d5ba1
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-07-26 14:10:42 +0700

    Menambahkan part 3 dan part 4

---
+++ Part_3 - Typedata/dasar.js
@@ -0,0 +1,27 @@
+// Cara membuat variable di javascript
+var a = 'a';
+let b = 'b';
+const c = 'c';
+
+// Typedata yang dipelajar saat ini = (string, integer, float, boolean)
+let nama = 'oskhar';     // String
+let umur = 22;           // Integer
+let belumMenikah = true; // Boolean
+let beratBadan = 50.4;   // Float
+
+// Operator aritmatika
+let angkaA = 10;
+let angkaB = 20;
+console.log(angkaA + angkaB);
+console.log(angkaA - angkaB);
+console.log(angkaA * angkaB);
+console.log(angkaA / angkaB);
+console.log(angkaA % angkaB);
+
+// Oprasi aritmatika bisa dibungkus atau dimasukan pada sebuah variable
+let hasilPerjumlahan = 10 + 10;
+console.log(hasilPerjumlahan);
+
+// Setelah melakukan deklarasi pada variabel tertentu, programmer tidak perlu melakukan deklarasi lagi (tidak pakai let dua kali)
+hasilPerjumlahan = 7 + 8
+console.log(hasilPerjumlahan);
---
+++ Part_4 - Push Github/README.md
@@ -0,0 +1,2 @@
+Pada tahap ini saya melakukan push ke github atau bisa disebut membuat repository yang menampung hasil belajar dari discord.
+pada repo ini nantinya akan saya tambahkan belajar per part dan mini wiki untuk detail referensi
