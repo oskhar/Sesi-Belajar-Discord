dee9edf7aec37799a423973376e4f3495b93954a
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-08-01 12:37:28 +0700

    Up Algoritma 1

--- Part_5 -  Pengkondisian/if-else.js
+++ Part_5 -  Pengkondisian/if-else.js
@@ -19,8 +19,22 @@
 //isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
 
 
-let nama = "",
+let nama = "Oskhar",
     peran = "";
 
 //code disini gunakan console.log untuk outputnya
+if (nama == '') {
+    console.log('Nama wajib diisi');
+    process.exit();
+}
 
+if (peran == '')
+    console.log('Pilih peranmu untuk memulai game');
+else if (peran == 'Ksatria')
+    console.log('halo Ksatria '+nama+' , kamu dapat menyerang dengan senjatamu!');
+else if (peran == 'Tabib')
+    console.log('halo Tabib '+nama+' , kamu akan membantu temanmu yang terluka');
+else if (peran == 'Penyihir')
+    console.log('halo Penyihir '+nama+' , ciptakan keajaiban yang membantu kemenanganmu!');
+else
+    console.log('kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada');
